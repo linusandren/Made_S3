e385ab5f746a0aa0a7401e0f0b4a2a7a7302d8d5
Jonathan Selander <jonathan@madepeople.se>
2015-08-15 16:42:33 +0200

    Allow overriding the resize cache url with a CDN url

--- src/code/S3/Model/Observer.php
+++ src/code/S3/Model/Observer.php
@@ -33,4 +33,28 @@ class Made_S3_Model_Observer
             Mage::logException($e);
         }
     }
+
+    /**
+     * Set the image cache CDN URL if entered in admin
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function getImageCdnUrl(Varien_Event_Observer $observer)
+    {
+        if (!Mage::getStoreConfig('system/s3/upload_resized_images')) {
+            return;
+        }
+
+        $cdnUrl = Mage::getStoreConfig('system/s3/cdn_url');
+        if (empty($cdnUrl)) {
+            return;
+        }
+
+        $result = $observer->getEvent()->getResult();
+        $imageInstance = $observer->getEvent()->getImageInstance();
+        $baseDir = Mage::getBaseDir();
+        $path = str_replace($baseDir . DS, "", $imageInstance->getNewFile());
+        $mediaUrl = $cdnUrl . $path;
+        $result->setUrl($mediaUrl);
+    }
 }
