27ebf8e8d23c0d4f95da3f3569d76303de335c24
Jonathan Selander <selander.jonathan@gmail.com>
2016-09-14 14:34:37 +0200

    Adjust key size for portability

--- src/code/S3/sql/made_s3_setup/mysql4-install-1.0.2.php
+++ src/code/S3/sql/made_s3_setup/mysql4-install-1.0.2.php
@@ -13,7 +13,7 @@ $this->run(<<<QUERY
 CREATE TABLE {$this->getTable('made_s3_resize_guard')} (
     original_image_path VARCHAR(4096) NOT NULL,
     resized_image_path VARCHAR(4096) NOT NULL,
-    PRIMARY KEY (original_image_path(380), resized_image_path(380))
+    PRIMARY KEY (original_image_path(200), resized_image_path(200))
 ) ENGINE=InnoDB;
 QUERY
 );
